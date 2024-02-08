46b57c40c563711acbe9b05e7c6645ed15c26db0
mahendra <solankirakesh895@gmail.com>
2024-02-08 19:11:03 +0530

    content updated

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const mobMenuSpan = document.querySelector(".mob-menu span");
-    const mainMenu = document.querySelector(".main-menu");
-  
-    mobMenuSpan.addEventListener("click", function() {
-      mainMenu.style.display = (mainMenu.style.display === "none" || mainMenu.style.display === "") ? "block" : "none";
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector(".scroll-hide");
+  const menus = document.querySelector(".menus");
+
+  let lastScrollTop = 0;
+
+  window.addEventListener("scroll", () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    const windowWidth = window.innerWidth;
+    if (scrollTop === 0) {
+      header.classList.remove("hidden-header");
+      menus.style.marginTop = "0";
+    } else {
+      if (scrollTop > lastScrollTop) {
+        header.classList.add("hidden-header");
+        menus.style.marginTop = "-70px";
+       
+      } else {
+        header.classList.remove("hidden-header");
+
+        menus.style.marginTop = "0";
+       
+      }
+    }
   });
-  
+});
